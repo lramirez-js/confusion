8465a234bf2436da08a25999f4836dc309e26521
cannedQualia <luis.rg@outlook.com>
2019-12-27 13:40:28 -0500

    SPA Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import Home from './HomeComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import { DISHES } from '../shared/dishes'
-import { Switch, Route, redirect, Redirect } from 'react-router-dom';
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -14,7 +18,10 @@ class Main extends Component {
         super(props);
 
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            leaders: LEADERS,
+            comments: COMMENTS,
+            promotions: PROMOTIONS
         };
     }
 
@@ -25,7 +32,10 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return(
-                <Home />
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]} 
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
             )
         }
 
@@ -34,6 +44,7 @@ class Main extends Component {
                 <Header />
                 <Switch>
                     <Route path="/home" component={HomePage} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Redirect to="/home" />
                 </Switch>
