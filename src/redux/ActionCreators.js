7511ab56fa899921d27ba2f5691c26e7539cd0a9
cannedQualia <luis.rg@outlook.com>
2020-01-21 19:15:46 -0500

    Redux Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,29 @@ export const addComment = (dishId, rating, author, comment) => ({
         author,
         comment
     }
+});
+
+// This is a THUNK, because we are returning a function:
+export const fetchDishes = () => (dispatch) => {
+    // We dispatch the action
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000)
+}
+
+// Function that returns 
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
 });
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,18 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from "./ActionTypes";
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+        isLoading: true,
+        errMes: null,
+        dishes: []
+    }, action) => {
     switch(action.type) {
+        case ActionTypes.ADD_DISHES:
+            return {...state, isLoading: false, errMes: null, dishes: action.payload}
+        case ActionTypes.DISHES_LOADING:
+            return {...state, isLoading: true, errMes: null, dishes: []}
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: false, errMes: action.payload(), dishes: []}
         default:
-        return state;
+            return state;
     }
 }
